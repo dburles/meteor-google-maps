31257c74d78a7ee586a2f9857084a59ebf59ff9d
Matt Krick <matt.krick@gmail.com>
2015-01-28 15:27:35 -0600

    Add created flag

--- google-maps.js
+++ google-maps.js
@@ -1,48 +1,45 @@
 GoogleMaps = {
-  load: _.once(function(library, key) {
+  load: _.once(function(options) {
+    options = options || {};
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
-      'callback=GoogleMaps.initialize';
-    if (library)
-      script.src += '&library=' + library;
-    if (key)
-      script.src += '&key=' + key;
-
+    'callback=GoogleMaps._initialize';
+    if (options.libraries)
+      script.src += '&libraries=' + options.libraries;
+    if (options.key)
+      script.src += '&key=' + options.key;
     document.body.appendChild(script);
   }),
   _loaded: new ReactiveVar(false),
+  _created: new ReactiveDict(),
   loaded: function() {
     return this._loaded.get();
   },
   maps: {},
-  _callbacks: {},
-  initialize: function() {
+  _initialize: function() {
     this._loaded.set(true);
   },
-  _ready: function(name, map) {
-    if (_.isFunction(this._callbacks[name]))
-      this._callbacks[name](map);
-  },
   ready: function(name, cb) {
-    this._callbacks[name] = cb;
+    if (_.isFunction(cb)) {
+      var self = this;
+      Template.instance().autorun(function() {
+        if (GoogleMaps._created.get(name)) {
+          cb(self.maps[name]);
+        }
+      });
+    } // else throw error?
   },
-  // options: function(options) {
-  //   var self = this;
-  //   return function() {
-  //     if (self.loaded())
-  //       return options();
-  //   };
-  // },
   get: function(name) {
     return this.maps[name];
   },
   _create: function(name, options) {
     this.maps[name] = {
       instance: options.instance,
-      options: options.options
+      options: options.options,
     };
-    this._ready(name, this.maps[name]);
+    //helper is loaded & template is created
+    this._created.set(name,true);
   }
 };
 
@@ -52,12 +49,13 @@ Template.googleMap.rendered = function() {
     // if the api has loaded
     if (GoogleMaps.loaded()) {
       var data = Template.currentData();
-      
       if (! data.name)
         throw new Meteor.Error("GoogleMaps - Missing argument: name");
       if ($.isEmptyObject(data.options))
         throw new Meteor.Error("GoogleMaps - Missing argument: options");
-      
+      if (!(data.options instanceof Object))
+        throw new Meteor.Error("GoogleMaps - options argument is not an object");
+
       var canvas = self.$('.map-canvas').get(0);
 
       GoogleMaps._create(data.name, {
