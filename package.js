e7590b29fe63a43645e030a2a9ea173c5c0c62f0
David Burles <dburles@gmail.com>
2015-05-14 16:47:44 +1000

    1.1.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dburles:google-maps',
   summary: 'Google Maps Javascript API v3',
-  version: '1.0.9',
+  version: '1.1.0',
   git: 'https://github.com/dburles/meteor-google-maps.git'
 });
 
